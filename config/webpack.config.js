a0ef71d11dd4d350199ec3a79980ca950308e812
Steve Rogers <49033503+Steve-Rog@users.noreply.github.com>
2022-01-11 13:54:19 +0100

    npm fix

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: path.resolve(__dirname + "./../src/index.js"),
   output: {
     path: path.resolve(__dirname + "./../dist"),
-    filename: "v-mintbutton.js",
+    filename: "vue-mint-button.js",
   },
   resolve: {
     fallback: {
