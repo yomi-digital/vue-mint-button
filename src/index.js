ad439f89484a1870b0f57b2750cf6b94d16e8e18
Steve Rogers <49033503+Steve-Rog@users.noreply.github.com>
2022-01-11 14:39:29 +0100

    fixing export

--- src/index.js
+++ src/index.js
@@ -1,26 +1,2 @@
 import MintButton from "./MintButton.vue";
-
-function install(Vue) {
-  if (install.installed) return;
-  install.installed = true;
-  Vue.component("v-mintbutton", MintButton);
-}
-
-const plugin = {
-  install,
-};
-
-let GlobalVue = null;
-if (typeof window !== "undefined") {
-  GlobalVue = window.Vue;
-} else if (typeof global !== "undefined") {
-  GlobalVue = global.vue;
-}
-
-if (GlobalVue) {
-  GlobalVue.use(plugin);
-}
-
-MintButton.install = install;
-
 export default MintButton;
